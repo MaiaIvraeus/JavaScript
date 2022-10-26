06c89d12d614a5329f2b1cd4188a5cdd5c03bddf
Maia Ivraeus <maria.ivraeus@chasacademy.se>
2022-10-26 08:45:11 +0200

    Test

--- main.js
+++ main.js
@@ -1,2 +1,40 @@
 var text = ("hello world")
 console.log(text);
+
+let mittNamn = "Maia";
+
+console.log(mittNamn);
+
+
+//Loop
+
+let number = 0;
+while (number <= 12) {
+    console.log(number)
+    number = number + 2;
+}
+
+
+
+
+//The logical NOT operator: !
+
+let a = false;
+    b = true;
+
+    console.log(!a);
+    console.log(!b);
+
+//The logical AND operator: &&
+    
+let c = false;
+    d = true;
+
+    console.log(c && d);
+
+    //The logical OR operator (||)
+
+let e = true;
+    f = false;
+
+console.log(a || b)
